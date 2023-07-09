9a31dbdbaf55b81243ffd5a593a4fa8a3ee90406
minft <klm0225@naver.com>
2023-07-09 17:04:27 +0900

    commit 2023-07-09 study

--- src/main/java/minft/minftspring/SpringConfig.java
+++ src/main/java/minft/minftspring/SpringConfig.java
@@ -1,14 +1,23 @@
 package minft.minftspring;
 
+import minft.minftspring.repository.JdbcMemberRepository;
 import minft.minftspring.repository.MemberRepository;
 import minft.minftspring.repository.MemoryMemberRepository;
 import minft.minftspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
